52f81229c85ff31aaed5979e51829dafd3fac641
skalar <19677138+NuclearPhone@users.noreply.github.com>
2022-12-19 08:34:30 -0500

    updates formatting, fixes test.c

--- test.c
+++ test.c
@@ -1,56 +1,58 @@
 #include "testc/test.h"
 #include <string.h>
+
+#define STR_IMPLEMENTATION
 #include "str.h"
 
-static const char* strcmp_test() {
-    str_t str1 = str_from_cstr_clone("hello");
-    str_t str2 = str_from_cstr_clone("hello");
-    str_t str3 = str_from_cstr_clone("hi there");
+static const char *strcmp_test() {
+  str_t str1 = str_from_cstr_clone("hello");
+  str_t str2 = str_from_cstr_clone("hello");
+  str_t str3 = str_from_cstr_clone("hi there");
 
-    if (!str_cmp(&str1, &str2)) {
-        return TESTC_BASIC_ERR;
-    }
+  if (!str_cmp(&str1, &str2)) {
+    return TESTC_BASIC_ERR;
+  }
 
-    str_destroy(&str1);
-    str_destroy(&str2);
-    str_destroy(&str3);
-    return NULL;
+  str_destroy(&str1);
+  str_destroy(&str2);
+  str_destroy(&str3);
+  return NULL;
 }
 
-static const char* appending_test() {
-    str_t str1 = str_from_cstr_clone("hello, ");
-    str_t str2 = str_from_cstr_clone("there!");
-    str_t expected = str_from_cstr_clone("hello, there!");
+static const char *appending_test() {
+  str_t str1 = str_from_cstr_clone("hello, ");
+  str_t str2 = str_from_cstr_clone("there!");
+  str_t expected = str_from_cstr_clone("hello, there!");
 
-    str_append(&str1, &str2);
+  str_append(&str1, &str2);
 
-    if (!str_cmp(&str1, &expected)) {
-        return TESTC_BASIC_ERR;
-    }
+  if (!str_cmp(&str1, &expected)) {
+    return TESTC_BASIC_ERR;
+  }
 
-    str_destroy(&str1);
-    str_destroy(&str2);
-    str_destroy(&expected);
+  str_destroy(&str1);
+  str_destroy(&str2);
+  str_destroy(&expected);
 
-    return NULL;
+  return NULL;
 }
 
-static const char* move_test() {
-    const char* _from = "hello";
-    char* from = malloc(sizeof "hello");
-    strcpy(from, _from);
+static const char *move_test() {
+  const char *_from = "hello";
+  char *from = malloc(sizeof "hello");
+  strcpy(from, _from);
 
-    str_t expected = str_from_cstr_clone("hello");
+  str_t expected = str_from_cstr_clone("hello");
 
-    str_t into = str_from_cstr_move(&from);
-    if (!str_cmp(&into, &expected)) {
-        return TESTC_BASIC_ERR;
-    }
+  str_t into = str_from_cstr_move(&from);
+  if (!str_cmp(&into, &expected)) {
+    return TESTC_BASIC_ERR;
+  }
 
-    str_destroy(&expected);
-    str_destroy(&into);
+  str_destroy(&expected);
+  str_destroy(&into);
 
-    return NULL;
+  return NULL;
 }
 
 const test_t tests[] = {
@@ -74,18 +76,18 @@ const test_t tests[] = {
 };
 
 extern int main() {
-    bool passed = true;
+  bool passed = true;
 
-    for (uint32_t i = 0; i < sizeof(tests) / sizeof(test_t); i++) {
-        test_t test = tests[i];
-        passed &= execute_test(test);
-    }
+  for (uint32_t i = 0; i < sizeof(tests) / sizeof(test_t); i++) {
+    test_t test = tests[i];
+    passed &= execute_test(test);
+  }
 
-    printf("\n");
+  printf("\n");
 
-    if (!passed) {
-        printf("SANITY CHECK FAILED\n");
-    } else {
-        printf("sanity check passed\n");
-    }
+  if (!passed) {
+    printf("SANITY CHECK FAILED\n");
+  } else {
+    printf("sanity check passed\n");
+  }
 }
