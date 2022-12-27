c1ef685cd1bc79777ec1ab7699e8e662f61ec95d
Skalar <19677138+NuclearPhone@users.noreply.github.com>
2022-12-27 17:47:26 -0500

    adds another append function & a cmp_cstr func

--- str.h
+++ str.h
@@ -11,26 +11,30 @@
 #endif
 
 typedef struct {
-  char *ptr;
+    char* ptr;
 
-  uint32_t capacity;
-  uint32_t len;
+    uint32_t capacity;
+    uint32_t len;
 } str_t;
 
-#define STR_STATIC_DEFN(str)                                                   \
-  (str_t) { .ptr = str, .capacity = sizeof(str) - 1, .len = sizeof(str) - 1 }
+#define STR_STATIC_DEFN(str)                                            \
+    (str_t) {                                                           \
+        .ptr = str, .capacity = sizeof(str) - 1, .len = sizeof(str) - 1 \
+    }
 
 STR_EXPORT str_t str_create();
-STR_EXPORT str_t str_from_cstr_move(char **);
-STR_EXPORT str_t str_from_cstr_clone(const char *);
-STR_EXPORT const char *str_cstr(const str_t *);
-STR_EXPORT void str_destroy(str_t *);
-STR_EXPORT bool str_cmp(const str_t *, const str_t *);
-STR_EXPORT void str_append_char(str_t *, char);
-STR_EXPORT void str_append(str_t *, str_t *from);
-STR_EXPORT void str_insert_char(str_t *, char, uint32_t idx);
-STR_EXPORT void str_insert(str_t *, str_t *from, uint32_t idx);
-STR_EXPORT char str_pop(str_t *);
+STR_EXPORT str_t str_from_cstr_move(char**);
+STR_EXPORT str_t str_from_cstr_clone(const char*);
+STR_EXPORT const char* str_cstr(const str_t*);
+STR_EXPORT void str_destroy(str_t*);
+STR_EXPORT bool str_cmp(const str_t*, const str_t*);
+STR_EXPORT bool str_cmp_cstr(const str_t*, const char*);
+STR_EXPORT void str_append_char(str_t*, char);
+STR_EXPORT void str_append_cstr(str_t*, const char*);
+STR_EXPORT void str_append(str_t*, str_t* from);
+STR_EXPORT void str_insert_char(str_t*, char, uint32_t idx);
+STR_EXPORT void str_insert(str_t*, str_t* from, uint32_t idx);
+STR_EXPORT char str_pop(str_t*);
 
 #ifndef DEFAULT_STR_CAPACITY
 #define DEFAULT_STR_CAPACITY 32
@@ -44,129 +48,153 @@ STR_EXPORT char str_pop(str_t *);
 
 #define STR_UNUSED(x) (void)x
 
-static inline uint32_t str_cstrlen(const char *in) {
-  uint32_t len = 0;
-  while (in[len] != 0)
-    len += 1;
-  return len;
+static inline uint32_t str_cstrlen(const char* in) {
+    uint32_t len = 0;
+    while (in[len] != 0)
+        len += 1;
+    return len;
 }
 
 // continuously resizes a strings capacity until it is >= than fit_cap
-static inline void str_fit(str_t *str, uint32_t fit_cap) {
-  uint32_t cap = str->capacity;
+static inline void str_fit(str_t* str, uint32_t fit_cap) {
+    uint32_t cap = str->capacity;
 
-  while (cap <= fit_cap) {
-    cap *= DEFAULT_STR_GROWTH_FACTOR;
-  }
+    while (cap <= fit_cap) {
+        cap *= DEFAULT_STR_GROWTH_FACTOR;
+    }
 
-  str->capacity = cap;
-  str->ptr = realloc(str->ptr, cap);
+    str->capacity = cap;
+    str->ptr = realloc(str->ptr, cap);
 }
 
 STR_EXPORT str_t str_create() {
-  const str_t out = {
-      .ptr = malloc(DEFAULT_STR_CAPACITY),
-      .capacity = DEFAULT_STR_CAPACITY,
-      .len = 0,
-  };
+    const str_t out = {
+        .ptr = malloc(DEFAULT_STR_CAPACITY),
+        .capacity = DEFAULT_STR_CAPACITY,
+        .len = 0,
+    };
 
-  return out;
+    return out;
 }
 
-STR_EXPORT const char *str_cstr(const str_t *in) {
-  if (in->ptr[in->len] != 0)
-    in->ptr[in->len] = 0;
+STR_EXPORT const char* str_cstr(const str_t* in) {
+    if (in->ptr[in->len] != 0)
+        in->ptr[in->len] = 0;
 
-  return in->ptr;
+    return in->ptr;
 }
 
 // clones the cstr
-STR_EXPORT str_t str_from_cstr_clone(const char *in) {
-  const uint32_t len = str_cstrlen(in);
+STR_EXPORT str_t str_from_cstr_clone(const char* in) {
+    const uint32_t len = str_cstrlen(in);
 
-  char *clone = malloc(len);
-  for (uint32_t i = 0; i < len; i++)
-    clone[i] = in[i];
+    char* clone = malloc(len);
+    for (uint32_t i = 0; i < len; i++)
+        clone[i] = in[i];
 
-  const str_t out = {
-      .ptr = clone,
-      .capacity = len,
-      .len = len,
-  };
+    const str_t out = {
+        .ptr = clone,
+        .capacity = len,
+        .len = len,
+    };
 
-  return out;
+    return out;
 }
 
-STR_EXPORT str_t str_from_cstr_move(char **in) {
-  char *cstr = *in;
-  *in = NULL;
+STR_EXPORT str_t str_from_cstr_move(char** in) {
+    char* cstr = *in;
+    *in = NULL;
 
-  const uint32_t len = str_cstrlen(cstr);
-  const str_t out = {
-      .ptr = cstr,
-      .capacity = len,
-      .len = len,
-  };
+    const uint32_t len = str_cstrlen(cstr);
+    const str_t out = {
+        .ptr = cstr,
+        .capacity = len,
+        .len = len,
+    };
 
-  return out;
+    return out;
 }
 
-STR_EXPORT void str_destroy(str_t *str) {
-  if (str->ptr)
-    free(str->ptr);
+STR_EXPORT void str_destroy(str_t* str) {
+    if (str->ptr)
+        free(str->ptr);
 }
 
-STR_EXPORT bool str_cmp(const str_t *left, const str_t *right) {
-  if (left->len != right->len)
-    return false;
+STR_EXPORT bool str_cmp(const str_t* left, const str_t* right) {
+    if (left->len != right->len)
+        return false;
 
-  for (uint32_t i = 0; i < left->len; i++)
-    if (left->ptr[i] != right->ptr[i])
-      return false;
+    for (uint32_t i = 0; i < left->len; i++)
+        if (left->ptr[i] != right->ptr[i])
+            return false;
 
-  return true;
+    return true;
 }
 
-STR_EXPORT void str_append_char(str_t *str, char c) {
-  str_fit(str, str->len + 1);
-  str->ptr[str->len] = c;
-  str->len += 1;
+STR_EXPORT bool str_cmp_cstr(const str_t* left, const char* right) {
+    const uint32_t right_len = str_cstrlen(right);
+
+    if (left->len != right_len)
+        return false;
+
+    for (uint32_t i = 0; i < left->len; i++)
+        if (left->ptr[i] != right[i])
+            return false;
+
+    return true;
 }
 
-STR_EXPORT void str_append(str_t *str, str_t *from) {
-  const uint32_t final_len = str->len + from->len;
-
-  str_fit(str, final_len);
-
-  for (uint32_t i = 0; i < from->len; i++)
-    str->ptr[i + str->len] = from->ptr[i];
-
-  str->len += from->len;
+STR_EXPORT void str_append_char(str_t* str, char c) {
+    str_fit(str, str->len + 1);
+    str->ptr[str->len] = c;
+    str->len += 1;
 }
 
-STR_EXPORT void str_insert_char(str_t *str, char c, uint32_t idx) {
-  str_fit(str, str->len += 1);
+STR_EXPORT void str_append_cstr(str_t* str, const char* from) {
+    const uint32_t from_len = str_cstrlen(from);
+    const uint32_t final_len = str->len + from_len;
 
-  for (uint32_t i = str->len; i > idx; i--)
-    str->ptr[i] = str->ptr[i - 1];
+    str_fit(str, final_len);
+    for (uint32_t i = 0; i < from_len; i++)
+        str->ptr[i + str->len] = from[i];
 
-  str->ptr[idx] = c;
+    str->len += from_len;
 }
 
-STR_EXPORT void str_insert(str_t *str, str_t *from, uint32_t idx) {
-  const uint32_t final_len = str->len + from->len;
+STR_EXPORT void str_append(str_t* str, str_t* from) {
+    const uint32_t final_len = str->len + from->len;
 
-  str_fit(str, final_len);
+    str_fit(str, final_len);
 
-  // move all of the characters that are displaced over
-  for (uint32_t i = final_len; i >= idx; i--)
-    str->ptr[i] = str->ptr[i - from->len];
+    for (uint32_t i = 0; i < from->len; i++)
+        str->ptr[i + str->len] = from->ptr[i];
 
-  // now copy the other string into its new place
-  for (uint32_t i = str->len; i < final_len; i++)
-    str->ptr[i] = from->ptr[i];
+    str->len += from->len;
+}
 
-  str->len = final_len;
+STR_EXPORT void str_insert_char(str_t* str, char c, uint32_t idx) {
+    str_fit(str, str->len += 1);
+
+    for (uint32_t i = str->len; i > idx; i--)
+        str->ptr[i] = str->ptr[i - 1];
+
+    str->ptr[idx] = c;
+}
+
+STR_EXPORT void str_insert(str_t* str, str_t* from, uint32_t idx) {
+    const uint32_t final_len = str->len + from->len;
+
+    str_fit(str, final_len);
+
+    // move all of the characters that are displaced over
+    for (uint32_t i = final_len; i >= idx; i--)
+        str->ptr[i] = str->ptr[i - from->len];
+
+    // now copy the other string into its new place
+    for (uint32_t i = str->len; i < final_len; i++)
+        str->ptr[i] = from->ptr[i];
+
+    str->len = final_len;
 }
 
 #endif
--- test.c
+++ test.c
@@ -4,106 +4,132 @@
 #define STR_IMPLEMENTATION
 #include "str.h"
 
-static const char *strcmp_test() {
-  str_t str1 = str_from_cstr_clone("hello");
-  str_t str2 = str_from_cstr_clone("hello");
-  str_t str3 = str_from_cstr_clone("hi there");
+static const char* strcmp_test() {
+    str_t str1 = str_from_cstr_clone("hello");
+    str_t str2 = str_from_cstr_clone("hello");
+    str_t str3 = str_from_cstr_clone("hi there");
 
-  if (!str_cmp(&str1, &str2)) {
-    return TESTC_BASIC_ERR;
-  }
+    if (!str_cmp(&str1, &str2)) {
+        return TESTC_BASIC_ERR;
+    }
 
-  str_destroy(&str1);
-  str_destroy(&str2);
-  str_destroy(&str3);
-  return NULL;
+    if (!str_cmp_cstr(&str1, "hello"))
+        return TESTC_BASIC_ERR;
+
+    str_destroy(&str1);
+    str_destroy(&str2);
+    str_destroy(&str3);
+    return NULL;
 }
 
-static const char *appending_test() {
-  str_t str1 = str_from_cstr_clone("hello, ");
-  str_t str2 = str_from_cstr_clone("there!");
-  str_t expected = str_from_cstr_clone("hello, there!");
+static const char* appending_test() {
+    str_t str1 = str_from_cstr_clone("hello, ");
+    str_t str2 = str_from_cstr_clone("there!");
+    str_t expected = str_from_cstr_clone("hello, there!");
 
-  str_append(&str1, &str2);
+    str_append(&str1, &str2);
 
-  if (!str_cmp(&str1, &expected)) {
-    return TESTC_BASIC_ERR;
-  }
+    if (!str_cmp(&str1, &expected)) {
+        return TESTC_BASIC_ERR;
+    }
 
-  str_destroy(&str1);
-  str_destroy(&str2);
-  str_destroy(&expected);
+    str_destroy(&str1);
+    str_destroy(&str2);
+    str_destroy(&expected);
 
-  return NULL;
+    return NULL;
 }
 
-static const char *move_test() {
-  const char *_from = "hello";
-  char *from = malloc(sizeof "hello");
-  strcpy(from, _from);
+static const char* move_test() {
+    const char* _from = "hello";
+    char* from = malloc(sizeof "hello");
+    strcpy(from, _from);
 
-  str_t expected = str_from_cstr_clone("hello");
+    str_t expected = str_from_cstr_clone("hello");
 
-  str_t into = str_from_cstr_move(&from);
-  if (!str_cmp(&into, &expected)) {
-    return TESTC_BASIC_ERR;
-  }
+    str_t into = str_from_cstr_move(&from);
+    if (!str_cmp(&into, &expected)) {
+        return TESTC_BASIC_ERR;
+    }
 
-  str_destroy(&expected);
-  str_destroy(&into);
+    str_destroy(&expected);
+    str_destroy(&into);
 
-  return NULL;
+    return NULL;
 }
 
-static const char *static_test() {
-  const str_t str = STR_STATIC_DEFN("hello");
-  str_t str_2 = str_from_cstr_clone("hello");
+static const char* static_test() {
+    const str_t str = STR_STATIC_DEFN("hello");
+    str_t str_2 = str_from_cstr_clone("hello");
 
-  if (!str_cmp(&str, &str_2)) {
-    return TESTC_BASIC_ERR;
-  }
+    if (!str_cmp(&str, &str_2)) {
+        return TESTC_BASIC_ERR;
+    }
 
-  str_destroy(&str_2);
-  return NULL;
+    str_destroy(&str_2);
+    return NULL;
 }
 
-const test_t tests[] = {(test_t){
-                            .ptr = strcmp_test,
-                            .name = "strcmp",
-                            .desc = "",
-                        },
+static const char* append_cstr() {
+    str_t str = str_from_cstr_clone("hi, ");
+    str_append_cstr(&str, "there!");
 
-                        (test_t){
-                            .ptr = appending_test,
-                            .name = "appending",
-                            .desc = "",
-                        },
+    str_t str_2 = str_from_cstr_clone("hi, there!");
 
-                        (test_t){
-                            .ptr = move_test,
-                            .name = "moving cstrs",
-                            .desc = "",
-                        },
+    if (!str_cmp(&str, &str_2)) {
+        return TESTC_BASIC_ERR;
+    }
 
-                        (test_t){
-                            .ptr = static_test,
-                            .name = "static test",
-                            .desc = "",
-                        }};
+    str_destroy(&str);
+    str_destroy(&str_2);
+    return NULL;
+}
+
+const test_t tests[] = {
+    (test_t){
+        .ptr = strcmp_test,
+        .name = "strcmp",
+        .desc = "",
+    },
+
+    (test_t){
+        .ptr = appending_test,
+        .name = "appending",
+        .desc = "",
+    },
+
+    (test_t){
+        .ptr = move_test,
+        .name = "moving cstrs",
+        .desc = "",
+    },
+
+    (test_t){
+        .ptr = static_test,
+        .name = "static test",
+        .desc = "",
+    },
+
+    (test_t){
+        .ptr = append_cstr,
+        .name = "append cstr",
+        .desc = "",
+    },
+};
 
 extern int main() {
-  bool passed = true;
+    bool passed = true;
 
-  for (uint32_t i = 0; i < sizeof(tests) / sizeof(test_t); i++) {
-    test_t test = tests[i];
-    passed &= execute_test(test);
-  }
+    for (uint32_t i = 0; i < sizeof(tests) / sizeof(test_t); i++) {
+        test_t test = tests[i];
+        passed &= execute_test(test);
+    }
 
-  printf("\n");
+    printf("\n");
 
-  if (!passed) {
-    printf("SANITY CHECK FAILED\n");
-  } else {
-    printf("sanity check passed\n");
-  }
+    if (!passed) {
+        printf("SANITY CHECK FAILED\n");
+    } else {
+        printf("sanity check passed\n");
+    }
 }
