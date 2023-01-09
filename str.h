18cc88675d5b55472a88d597bede4f5d9dd10f84
skalar <19677138+NuclearPhone@users.noreply.github.com>
2023-01-09 09:52:52 -0500

    sets a string to be null if destroyed

--- str.h
+++ str.h
@@ -14,45 +14,43 @@
 #endif
 
 typedef struct {
-    char* ptr;
+    char *ptr;
 
     uint32_t capacity;
     uint32_t len;
 } str_t;
 
-#define STR_OPTIONAL(val) \
-    struct {              \
-        val v;            \
+#define STR_OPTIONAL(val)                                                      \
+    struct {                                                                   \
+        val v;                                                                 \
     }
 
-#define STR_STATIC_DEFN(str)                                            \
-    (str_t) {                                                           \
-        .ptr = str, .capacity = sizeof(str) - 1, .len = sizeof(str) - 1 \
-    }
+#define STR_STATIC_DEFN(str)                                                   \
+    (str_t) { .ptr = str, .capacity = sizeof(str) - 1, .len = sizeof(str) - 1 }
 
 STR_EXPORT str_t str_create();
-STR_EXPORT str_t str_generate(const char* format, ...);
-STR_EXPORT str_t str_clone(const str_t* from);
-STR_EXPORT str_t str_from_cstr_move(char**);
-STR_EXPORT str_t str_from_cstr_clone(const char*);
+STR_EXPORT str_t str_generate(const char *format, ...);
+STR_EXPORT str_t str_clone(const str_t *from);
+STR_EXPORT str_t str_from_cstr_move(char **);
+STR_EXPORT str_t str_from_cstr_clone(const char *);
 // end must be one after whatever you want to copy
-STR_EXPORT str_t str_from_range(const char* begin, const char* end);
-STR_EXPORT const char* str_cstr(const str_t*);
-STR_EXPORT void str_destroy(str_t*);
-STR_EXPORT bool str_cmp(const str_t*, const str_t*);
-STR_EXPORT bool str_cmp_cstr(const str_t*, const char*);
-STR_EXPORT void str_append_char(str_t*, const char);
-STR_EXPORT void str_append_cstr(str_t*, const char*);
-STR_EXPORT void str_append(str_t*, const str_t* from);
-STR_EXPORT void str_insert_char(str_t*, const char, const uint32_t idx);
-STR_EXPORT void str_insert(str_t*, const str_t* from, const uint32_t idx);
-STR_EXPORT char str_pop(str_t*);
+STR_EXPORT str_t str_from_range(const char *begin, const char *end);
+STR_EXPORT const char *str_cstr(const str_t *);
+STR_EXPORT void str_destroy(str_t *);
+STR_EXPORT bool str_cmp(const str_t *, const str_t *);
+STR_EXPORT bool str_cmp_cstr(const str_t *, const char *);
+STR_EXPORT void str_append_char(str_t *, const char);
+STR_EXPORT void str_append_cstr(str_t *, const char *);
+STR_EXPORT void str_append(str_t *, const str_t *from);
+STR_EXPORT void str_insert_char(str_t *, const char, const uint32_t idx);
+STR_EXPORT void str_insert(str_t *, const str_t *from, const uint32_t idx);
+STR_EXPORT char str_pop(str_t *);
 
 // returns -1 for error
-STR_EXPORT int str_ip_to_long(const str_t*, uint32_t* out);
+STR_EXPORT int str_ip_to_long(const str_t *, uint32_t *out);
 
 // returns -1 for error
-STR_EXPORT int str_parse_integer(const str_t*, int64_t* out);
+STR_EXPORT int str_parse_integer(const str_t *, int64_t *out);
 
 #ifndef DEFAULT_STR_CAPACITY
 #define DEFAULT_STR_CAPACITY 32
@@ -66,20 +64,20 @@ STR_EXPORT int str_parse_integer(const str_t*, int64_t* out);
 
 #define STR_UNUSED(x) (void)x
 
-static inline uint32_t str_cstrlen(const char* in) {
+static inline uint32_t str_cstrlen(const char *in) {
     uint32_t len = 0;
     while (in[len] != 0)
         len += 1;
     return len;
 }
 
-static inline void str_memcpy(char* to, const char* from, uint32_t len) {
+static inline void str_memcpy(char *to, const char *from, uint32_t len) {
     for (uint32_t idx = 0; idx < len; idx++)
         to[idx] = from[idx];
 }
 
 // continuously resizes a strings capacity until it is >= than fit_cap
-static inline void str_fit(str_t* str, uint32_t fit_cap) {
+static inline void str_fit(str_t *str, uint32_t fit_cap) {
     uint32_t cap = str->capacity;
 
     while (cap <= fit_cap) {
@@ -100,13 +98,13 @@ STR_EXPORT str_t str_create() {
     return out;
 }
 
-STR_EXPORT str_t str_generate(const char* fmt, ...) {
+STR_EXPORT str_t str_generate(const char *fmt, ...) {
     va_list list, copy;
     va_start(list, fmt);
     va_copy(copy, list);
 
     uint32_t len = vsnprintf(NULL, 0, fmt, list);
-    char* ptr = malloc(len + 1);
+    char *ptr = malloc(len + 1);
     ptr[len] = 0;
     vsnprintf(ptr, len + 1, fmt, copy);
 
@@ -120,8 +118,8 @@ STR_EXPORT str_t str_generate(const char* fmt, ...) {
     };
 }
 
-STR_EXPORT str_t str_clone(const str_t* in) {
-    char* copy = malloc(in->len + 1);
+STR_EXPORT str_t str_clone(const str_t *in) {
+    char *copy = malloc(in->len + 1);
     copy[in->len] = 0;
     str_memcpy(copy, in->ptr, in->len);
     return (str_t){
@@ -131,7 +129,7 @@ STR_EXPORT str_t str_clone(const str_t* in) {
     };
 }
 
-STR_EXPORT const char* str_cstr(const str_t* in) {
+STR_EXPORT const char *str_cstr(const str_t *in) {
     if (in->ptr[in->len] != 0)
         in->ptr[in->len] = 0;
 
@@ -139,10 +137,10 @@ STR_EXPORT const char* str_cstr(const str_t* in) {
 }
 
 // clones the cstr
-STR_EXPORT str_t str_from_cstr_clone(const char* in) {
+STR_EXPORT str_t str_from_cstr_clone(const char *in) {
     const uint32_t len = str_cstrlen(in);
 
-    char* clone = malloc(len);
+    char *clone = malloc(len);
     for (uint32_t i = 0; i < len; i++)
         clone[i] = in[i];
 
@@ -155,8 +153,8 @@ STR_EXPORT str_t str_from_cstr_clone(const char* in) {
     return out;
 }
 
-STR_EXPORT str_t str_from_cstr_move(char** in) {
-    char* cstr = *in;
+STR_EXPORT str_t str_from_cstr_move(char **in) {
+    char *cstr = *in;
     *in = NULL;
 
     const uint32_t len = str_cstrlen(cstr);
@@ -169,9 +167,9 @@ STR_EXPORT str_t str_from_cstr_move(char** in) {
     return out;
 }
 
-STR_EXPORT str_t str_from_range(const char* begin, const char* end) {
+STR_EXPORT str_t str_from_range(const char *begin, const char *end) {
     const uint32_t len = end - begin;
-    char* copy = malloc(len);
+    char *copy = malloc(len);
     str_memcpy(copy, begin, len);
     return (str_t){
         .ptr = copy,
@@ -180,12 +178,14 @@ STR_EXPORT str_t str_from_range(const char* begin, const char* end) {
     };
 }
 
-STR_EXPORT void str_destroy(str_t* str) {
-    if (str->ptr)
+STR_EXPORT void str_destroy(str_t *str) {
+    if (str->ptr) {
         free(str->ptr);
+        str = NULL;
+    }
 }
 
-STR_EXPORT bool str_cmp(const str_t* left, const str_t* right) {
+STR_EXPORT bool str_cmp(const str_t *left, const str_t *right) {
     if (left->len != right->len)
         return false;
 
@@ -196,7 +196,7 @@ STR_EXPORT bool str_cmp(const str_t* left, const str_t* right) {
     return true;
 }
 
-STR_EXPORT bool str_cmp_cstr(const str_t* left, const char* right) {
+STR_EXPORT bool str_cmp_cstr(const str_t *left, const char *right) {
     const uint32_t right_len = str_cstrlen(right);
 
     if (left->len != right_len)
@@ -209,13 +209,13 @@ STR_EXPORT bool str_cmp_cstr(const str_t* left, const char* right) {
     return true;
 }
 
-STR_EXPORT void str_append_char(str_t* str, char c) {
+STR_EXPORT void str_append_char(str_t *str, char c) {
     str_fit(str, str->len + 1);
     str->ptr[str->len] = c;
     str->len += 1;
 }
 
-STR_EXPORT void str_append_cstr(str_t* str, const char* from) {
+STR_EXPORT void str_append_cstr(str_t *str, const char *from) {
     const uint32_t from_len = str_cstrlen(from);
     const uint32_t final_len = str->len + from_len;
 
@@ -226,7 +226,7 @@ STR_EXPORT void str_append_cstr(str_t* str, const char* from) {
     str->len += from_len;
 }
 
-STR_EXPORT void str_append(str_t* str, const str_t* from) {
+STR_EXPORT void str_append(str_t *str, const str_t *from) {
     const uint32_t final_len = str->len + from->len;
 
     str_fit(str, final_len);
@@ -237,7 +237,7 @@ STR_EXPORT void str_append(str_t* str, const str_t* from) {
     str->len += from->len;
 }
 
-STR_EXPORT void str_insert_char(str_t* str, const char c, const uint32_t idx) {
+STR_EXPORT void str_insert_char(str_t *str, const char c, const uint32_t idx) {
     str_fit(str, str->len += 1);
 
     for (uint32_t i = str->len; i > idx; i--)
@@ -246,7 +246,7 @@ STR_EXPORT void str_insert_char(str_t* str, const char c, const uint32_t idx) {
     str->ptr[idx] = c;
 }
 
-STR_EXPORT void str_insert(str_t* str, const str_t* from, const uint32_t idx) {
+STR_EXPORT void str_insert(str_t *str, const str_t *from, const uint32_t idx) {
     const uint32_t final_len = str->len + from->len;
 
     str_fit(str, final_len);
@@ -262,8 +262,8 @@ STR_EXPORT void str_insert(str_t* str, const str_t* from, const uint32_t idx) {
     str->len = final_len;
 }
 
-STR_EXPORT int str_ip_to_long(const str_t* str, uint32_t* out) {
-    const char* ptr = str->ptr;
+STR_EXPORT int str_ip_to_long(const str_t *str, uint32_t *out) {
+    const char *ptr = str->ptr;
 
     uint32_t final = 0;
 
@@ -297,8 +297,8 @@ STR_EXPORT int str_ip_to_long(const str_t* str, uint32_t* out) {
     return 0;
 }
 
-STR_EXPORT int str_parse_integer(const str_t* str, int64_t* out) {
-    const char* ptr = str->ptr;
+STR_EXPORT int str_parse_integer(const str_t *str, int64_t *out) {
+    const char *ptr = str->ptr;
 
     bool neg = ptr[0] == '-';
     int64_t acc = 0;
